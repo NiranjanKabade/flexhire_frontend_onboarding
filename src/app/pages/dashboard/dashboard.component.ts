f465086f6d332f0380f58f4f13af9e45e0e9ac17
SurajSwatsan <suraj.dinde@swatsan.com>
2024-10-17 18:06:04 +0530

    dashboard stepper changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject, ViewChild } from '@angular/core';
+import {
+  Component,
+  inject,
+  Type,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatStepper } from '@angular/material/stepper';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatStepperModule } from '@angular/material/stepper';
@@ -6,6 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
+
 import {
   FormBuilder,
   FormGroup,
@@ -22,7 +29,10 @@ import { StepperModule } from 'primeng/stepper';
 import { StepsModule } from 'primeng/steps';
 import { CompanyDocumentationComponent } from '../company-documentation/company-documentation.component';
 import { FormDataService } from '../../form-data.service';
-
+interface Step {
+  title: string;
+  component: Type<any>;
+}
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -40,56 +50,77 @@ import { FormDataService } from '../../form-data.service';
     CompanyDocumentationComponent,
     ButtonModule,
     StepperModule,
-    StepsModule
+    StepsModule,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
- 
-  activeIndex: number = 0;
-  steps = [
-    { label: 'Company Information'},
-    { label: 'Contact Information'},
-    { label: 'Company Documentation'},
+  steps: any = [
+    { title: 'Company Information', component: CompanyInfoComponent },
+    { title: 'Contact Information', component: ContactInfoComponent },
+    { title: 'Comapany Documents', component: CompanyDocumentationComponent },
+    { title: 'Done', content: 'Completed' },
   ];
+  activeStep = 0;
 
-  constructor(private formDataService: FormDataService) {}
+  goToStep(index: number) {
+    if (index === this.steps.length - 1) {
+      this.activeStep = index;
+    } else {
+      this.activeStep = index;
+    }
+  }
+  prevStep() {
+    if (this.activeStep > 0) {
+      this.activeStep--;
+    }
+  }
+  
+  nextStep() {
+    if (this.activeStep < this.steps.length - 1) {
+      this.activeStep++;
+    }
+  }
+  // activeIndex: number = 0;
+  // steps = [
+  //   { label: 'Company Information'},
+  //   { label: 'Contact Information'},
+  //   { label: 'Company Documentation'},
+  // ];
 
+  // constructor(private formDataService: FormDataService) {}
 
-  // saveCurrentFormData(step: string, data: any) {
-  //   // Save data to cache (BehaviorSubject) and localStorage
-  //   this.formDataService.setFormData(step, data);
+  // // saveCurrentFormData(step: string, data: any) {
+  // //   // Save data to cache (BehaviorSubject) and localStorage
+  // //   this.formDataService.setFormData(step, data);
+  // // }
+
+  // onNext(step: string, data: any) {
+  //   this.formDataService.updateFormData(step as keyof FormData, data);
+  //   if (this.activeIndex < this.steps.length - 1) {
+  //     this.activeIndex++;
+  //   }
   // }
 
-  
-  onNext(step: string, data: any) {
-    this.formDataService.updateFormData(step as keyof FormData, data);
-    if (this.activeIndex < this.steps.length - 1) {
-      this.activeIndex++;
-    }
-  }
+  // onPrevious() {
+  //   if (this.activeIndex > 0) {
+  //     this.activeIndex--;
+  //   }
+  // }
 
-
-  onPrevious() {
-    if (this.activeIndex > 0) {
-      this.activeIndex--;
-    }
-  }
+  // // submitForm() {
+  // //   this.formDataService.submitData().subscribe({
+  // //     next: (response) => {
+  // //       console.log('Form submitted successfully!', response);
+  // //     },
+  // //     error: (err) => {
+  // //       console.error('Error submitting form', err);
+  // //     },
+  // //   });
+  // // }
 
   // submitForm() {
-  //   this.formDataService.submitData().subscribe({
-  //     next: (response) => {
-  //       console.log('Form submitted successfully!', response);
-  //     },
-  //     error: (err) => {
-  //       console.error('Error submitting form', err);
-  //     },
-  //   });
+  //   // Delegate the submit action to the documentation component or directly handle here
   // }
-
-  submitForm() {
-    // Delegate the submit action to the documentation component or directly handle here
-  }
- 
 }
